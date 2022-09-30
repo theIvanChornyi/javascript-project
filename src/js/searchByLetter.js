021f2eebd8c03de41d50f7c0564c086cd41e8bfb
Farid Suleymanov <105884209+FaridMK@users.noreply.github.com>
2022-10-01 00:19:14 +0400

    Update searchByLetter.js

--- src/js/searchByLetter.js
+++ src/js/searchByLetter.js
@@ -201,7 +201,7 @@ function onInput(event) {
     wrireRemovetCoctaileFunction('[data-cocktaileId]');
     openCoctaileInfoModal('[data-moreId]');
   } catch (error) {}
-}
+
 
 function sorryText() {
   const markup = `
