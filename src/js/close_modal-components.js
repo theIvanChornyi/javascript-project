e9afa12477aef51e21a59e854219f3d26d777ae5
Ivan Chornyi <theivanchornyi@gmail.com>
2022-10-01 15:14:17 +0300

    temporary fix

--- src/js/close_modal-components.js
+++ src/js/close_modal-components.js
@@ -4,20 +4,14 @@ const markupRef = document.querySelector('.markup');
 const closeRef = document.querySelector('.description__button__close');
 const favoritBtRef = document.querySelector('.description__favoriteBt');
 
-// closeRef.addEventListener('click', () => {
-//   modalRef.classList.add('visually-hidden');
-// });
+// openModal();
 
-openModal();
-
-favoritBtRef.addEventListener('click', offFavoritBt);
+// favoritBtRef.addEventListener('click', offFavoritBt);
 
 function offFavoritBt(e) {
-  // body.overflow: hidden;
   favoritBtRef.innerHTML = `Remove from favorite`;
   const removeFaforit = document.querySelector('description__button');
   favoritBtRef.removeEventListener('click', offFavoritBt);
-  // removeFaforit.addEventListener('click', onFavoritBt);
 }
 
 function onFavoritBt(e) {
@@ -53,9 +47,8 @@ async function check(element) {
 }
 
 async function marcup(data) {
-  console.log('data', data);
-  return await data
-    .map(({ strIngredient, strDescription, strType, strABV }) => {
+  return await data.map(
+    ({ strIngredient, strDescription, strType, strABV }) => {
       let string = '';
       if (strIngredient) {
         string += `<h2 class="description__title">${strIngredient}</h2>`;
@@ -74,12 +67,13 @@ async function marcup(data) {
       if (strABV) {
         string += `<li class="description__list"><span class="description__accent">&#10038</span> Alcohol by volume: ${strABV}</li>`;
       }
-      // console.log(string);
-      const htmlElem = `${string}`;
-
+      return string;
+    }
+  );
+}
 
 function closeEsc(e) {
-  if (e.code === Escape) {
+  if (e.code === 'Escape') {
     modalRef.classList.add('visually-hidden');
   }
 }
@@ -89,10 +83,3 @@ function closeBacdrop(e) {
     modalRef.classList.add('visually-hidden');
   }
 }
-
-{
-  /* <ul class="description__item">
-  <li class="description__list">
-    <span class="description__accent">&#10038</span> Country of origin: Italy
-  </li>
-
