2daa650c9957e344c86695d03f551860e6c3d088
Ivan Chornyi <theivanchornyi@gmail.com>
2022-10-02 17:51:44 +0300

    temporary 2

--- src/js/close_modal-components.js
+++ src/js/close_modal-components.js
@@ -1,72 +1,64 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// export function openCoctaileInfoModal() {
-//   const favoriteBtn = document.querySelectorAll('[data-ingridientname]');
-//   favoriteBtn.forEach(btn => btn.addEventListener('click', openModal));
-// }
+export function openIngridientInfoModal() {
+  const favoriteBtn = document.querySelectorAll('[data-ingridientname]');
+  favoriteBtn.forEach(btn => btn.addEventListener('click', openModal));
+}
 
-// function openModal(e) {
-//   const IngrdName = e.currentTarget.dataset.ingridientname;
-//   getIngridient(IngrdName);
-//   // closeRef.addEventListener('click', () => {
-//   //   modalRef.classList.add('visually-hidden');
-//   // });
-//   // modalRef.addEventListener('click', closeBacdrop);
-//   // document.addEventListener('keydown', closeEsc);
-// }
+function openModal(e) {
+  const IngrdName = e.currentTarget.dataset.ingridientname;
+  getIngridient(IngrdName);
+  // closeRef.addEventListener('click', () => {
+  //   modalRef.classList.add('visually-hidden');
+  // });
+  // modalRef.addEventListener('click', closeBacdrop);
+  // document.addEventListener('keydown', closeEsc);
+}
 
-// async function getIngridient(IngrdName) {
-//   const request = await axios(
-//     `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${IngrdName}`
-//   ).then(({ data: { ingredients } }) => {
-//     check(ingredients);
-//   });
-// }
+async function getIngridient(IngrdName) {
+  const request = await axios(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${IngrdName}`
+  );
+  const objectData = await request.data.ingredients[0];
+  const createMarkup = await marcup(objectData);
+  const DOM = document.querySelector('.backdrop__cocktail');
+  DOM.insertAdjacentHTML('beforeend', createMarkup);
+}
 
-// async function check(element) {
-//   const markupString = await marcup(element);
-//   markupRef.innerHTML = markupString;
+async function marcup({
+  idIngredient,
+  strIngredient,
+  strDescription,
+  strType,
+  strABV,
+}) {
+  let string = '';
 
-//   const objectData = await request.data.ingredients[0];
-//   const createMarkup = await marcup(objectData);
-//   const DOM = document.querySelector('.backdrop__cocktail');
-//   DOM.insertAdjacentHTML('beforeend', createMarkup);
-// }
+  if (strIngredient) {
+    string += `<h2 class="description__title">${strIngredient}</h2>`;
+  }
+  if (strType) {
+    string += `<h3 class="description__category">${strType}</h3>`;
+  }
+  if (strDescription) {
+    string += `<p class="description__characteristic">
+      ${strDescription.substring(0, 404)}
+    </p>`;
+  }
+  if (strType) {
+    string += `<li class="description__list"><span class="description__accent">&#10038</span> Type: ${strType}</li>`;
+  }
+  if (strABV) {
+    string += `<li class="description__list"><span class="description__accent">&#10038</span> Alcohol by volume: ${strABV}</li>`;
+  }
+  return `
 
-// async function marcup({
-//   idIngredient,
-//   strIngredient,
-//   strDescription,
-//   strType,
-//   strABV,
-// }) {
-//   let string = '';
-
-//   if (strIngredient) {
-//     string += `<h2 class="description__title">${strIngredient}</h2>`;
-//   }
-//   if (strType) {
-//     string += `<h3 class="description__category">${strType}</h3>`;
-//   }
-//   if (strDescription) {
-//     string += `<p class="description__characteristic">
-//       ${strDescription.substring(0, 404)}
-//     </p>`;
-//   }
-//   if (strType) {
-//     string += `<li class="description__list"><span class="description__accent">&#10038</span> Type: ${strType}</li>`;
-//   }
-//   if (strABV) {
-//     string += `<li class="description__list"><span class="description__accent">&#10038</span> Alcohol by volume: ${strABV}</li>`;
-//   }
-//   return `
-
-//   <div class="description">
-//     <button type="button" autofocus class="description__button__close" data-modal="close-cocktail">
-//     </button>
-//   ${string}
-//   </div>
-//         <div class="cocktail__modal-btn">
-//         <button type="button" class="description__favoriteBt modal__btnJS" data-cocktaileid='${idIngredient}'>Add to favorite</button>
-//       </div>`;
-// }
+  <div class="description">
+    <button type="button" autofocus class="description__button__close" data-modal="close-cocktail">
+    </button>
+  ${string}
+  </div>
+        <div class="cocktail__modal-btn">
+        <button type="button" class="description__favoriteBt modal__btnJS" data-cocktaileid='${idIngredient}'>Add to favorite</button>
+      </div>`;
+}
